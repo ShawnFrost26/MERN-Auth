248668a79a11ea5786d415f96f0579238216f413
ShawnFrost26 <sb262202@gmail.com>
2025-01-03 15:33:53 +0530

    added controller auth signup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationToken } from "../utils/generateVerificationCode.js";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const verificationToken = generateVerificationToken();
+
+    const user = new User({
+      email,
+      password: hashedpassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+export const login = async (req, res) => {
+  res.send("login Route");
+};
+export const logout = async (req, res) => {
+  res.send("logout Route");
+};
