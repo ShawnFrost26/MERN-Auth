2519f8b90afe7df3eda41181bb6658453cee81fc
ShawnFrost26 <sb262202@gmail.com>
2025-01-06 11:42:29 +0530

    verify email route and controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
-import { sendVerificationEmail } from "../brevo/emails.js";
+import { sendVerificationEmail, sendWelcomeEmail } from "../brevo/emails.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -22,7 +22,7 @@ export const signup = async (req, res) => {
     const verificationToken = Math.floor(
       100000 + Math.random() * 90000
     ).toString();
-    console.log("verificationToken: ", verificationToken);
+    // console.log("verificationToken: ", verificationToken);
 
     const user = new User({
       email,
@@ -38,7 +38,7 @@ export const signup = async (req, res) => {
     generateTokenAndSetCookie(res, user._id);
 
     await sendVerificationEmail(user.email, user.name, verificationToken);
-    
+
     res.status(201).json({
       success: true,
       message: "User created successfully",
@@ -51,6 +51,43 @@ export const signup = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
+
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Invalid or expired verification code",
+        });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+
+    res.status(200).json({
+      success: true,
+      message: "Email verified successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {}
+};
+
 export const login = async (req, res) => {
   res.send("login Route");
 };
