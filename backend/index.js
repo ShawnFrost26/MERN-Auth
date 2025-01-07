565394cd910b9debc50d0bf18e0e46234b855a72
ShawnFrost26 <sb262202@gmail.com>
2025-01-07 18:01:56 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -9,8 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
