c64da43e30b7f1cdaf161a8062b70e8258c513df
ShawnFrost26 <sb262202@gmail.com>
2025-01-07 15:35:05 +0530

    implemented login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,66 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from "react";
+import { Mail, Lock } from "lucide-react";
+import { Link } from "react-router-dom";
+import Input from "../components/Input";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginPage
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="max-w-md w-full bg-gray-800 opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+      <div className="p-8">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-cyan-400 to-teal-500 text-transparent bg-clip-text">
+          Welcome Back
+        </h2>
+        <form onSubmit={handleLogin}>
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="flex items-center mb-6">
+            <Link
+              to="/forgot-password"
+              className="text-sm text-cyan-400 hover:underline"
+            >
+              Forgot password?
+            </Link>
+          </div>
+          <button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-cyan-500 to-teal-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-cyan-600
+						hover:to-teal-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-black bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-cyan-400 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
